d555090d4e4eea99aef6d0bbff32c71d0a5069b1
svianac <31249803+svianac@users.noreply.github.com>
2024-10-25 10:55:32 +0200

    Update list_clouds.groovy

---
+++ jenkins/list_clouds.groovy
@@ -0,0 +1,19 @@
+// Import Jenkins classes
+import jenkins.model.Jenkins
+
+// Get the Jenkins instance
+def jenkinsInstance = Jenkins.instance
+
+// Fetch the list of all configured clouds
+def clouds = jenkinsInstance.clouds
+
+// Check if there are any configured clouds
+if (clouds.isEmpty()) {
+    println "No clouds are configured in Jenkins."
+} else {
+    println "Configured clouds in Jenkins:"
+    clouds.each { cloud ->
+        // Print cloud details
+        println "- Name: ${cloud.name} (Class: ${cloud.getClass().getSimpleName()})"
+    }
+}
