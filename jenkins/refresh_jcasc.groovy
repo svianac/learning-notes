16b896f4c6bcba54cacba9b6a0e4c2d12f467fcb
svianac <31249803+svianac@users.noreply.github.com>
2024-10-28 08:16:40 +0100

    Update refresh_jcasc.groovy

--- jenkins/refresh_jcasc.groovy
+++ jenkins/refresh_jcasc.groovy
@@ -1,28 +1,16 @@
 jobs:
   - script: >
       job('reload-jcasc-configuration') {
-          description('Job to reload Jenkins Configuration as Code (JCasC) configuration.')
+          description('This job reloads the JCasC configuration.')
           steps {
-              dsl {
-                  scriptText('''
-                      job("reload-jcasc-configuration") {
-                          description("Reloads the JCasC configuration without restarting Jenkins.")
-                          steps {
-                              systemGroovyCommand('''
-                                  import jenkins.model.*
-                                  import io.jenkins.plugins.casc.ConfigurationAsCode
-
-                                  def jcasC = Jenkins.instance.getExtensionList(ConfigurationAsCode.class)[0]
-                                  if (jcasC) {
-                                      jcasC.configure()
-                                      println("JCasC configuration reloaded successfully.")
-                                  } else {
-                                      println("JCasC plugin is not available.")
-                                  }
-                              ''')
-                          }
-                      }
-                  ''')
-              }
+              systemGroovyCommand('''
+                  def jcasC = jenkins.model.Jenkins.instance.getExtensionList(io.jenkins.plugins.casc.ConfigurationAsCode.class)[0]
+                  if (jcasC) {
+                      jcasC.configure()
+                      println("JCasC configuration reloaded successfully.")
+                  } else {
+                      println("JCasC plugin is not available.")
+                  }
+              ''')
           }
       }
